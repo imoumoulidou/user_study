34236e4edd475205db3be8e235a212099996d9c2
Hamza Elhamdadi <helhamdadi@umass.edu>
2024-10-28 15:46:29 -0400

    added tour code

--- getImage.js
+++ getImage.js
@@ -97,6 +97,82 @@ function buildQuestion1(questionId){
 
 }
 
+function buildQuestion2(questionId){
+    let questionDiv = document.getElementById(questionId).getElementsByClassName('QuestionText')[0];
+
+    console.log(questionDiv)
+
+    let row = document.createElement('div');
+    row.className = 'row';
+
+    let leftColumn = document.createElement('div');
+    leftColumn.className = 'col-6';
+
+    let orig_image = document.createElement('img');
+    orig_image.id = 'orig';
+    orig_image.height = 1200;
+    orig_image.width = 1400;
+
+    let orig_slider = document.createElement('input');
+    orig_slider.type = 'range';
+    orig_slider.id = 'orig_alpha_slider';
+    orig_slider.className = 'form-range';
+    orig_slider.min = 1;
+    orig_slider.max = 10;
+    orig_slider.step = 3;
+    orig_slider.value = 10;
+
+    let orig_text = document.createElement('p');
+    orig_text.id = 'orig_alpha_text';
+    orig_text.innerHTML = 'Transparency: 1.0';
+
+    leftColumn.append(orig_image);
+    leftColumn.append(orig_slider);
+    leftColumn.append(orig_text);
+
+    let rightColumn = document.createElement('div');
+    rightColumn.className = 'col-6';
+
+    let sample_index = 1;
+    let sample_names = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+    for(let i = 0; i < 2; i++){
+        let new_row = document.createElement('div');
+        new_row.className = 'row';
+
+        for(let j = 0; j < 3; j++){
+            let new_col = document.createElement('div');
+            new_col.className = 'col-4 text-center';
+
+            let new_col_image = document.createElement('img');
+            new_col_image.id = 'sample_' + sample_index;
+            new_col_image.height = 600;
+            new_col_image.width = 700;
+
+            let new_col_text = document.createElement('h3');
+            new_col_text.innerHTML = sample_names[sample_index-1];
+
+            new_col.append(new_col_image);
+            new_col.append(new_col_text);
+
+            new_row.append(new_col);
+
+            sample_index++;
+        }
+
+        rightColumn.append(new_row);
+    }
+    
+
+    row.append(leftColumn);
+    row.append(rightColumn);
+
+    console.log(row)
+
+    questionDiv.append(row);
+
+}
+
 
 function setupQuestion(questionNum, questionId){
     if(questionNum == 1) buildQuestion1(questionId);
---
+++ tour.js
@@ -0,0 +1,95 @@
+function createTour(steps){
+  
+    let tour = new Shepherd.Tour({
+      useModalOverlay: true,
+      defaultStepOptions: {
+        cancelIcon: {
+          enabled: true
+        },
+        classes: 'class-1 class-2',
+        scrollTo: {behavior: 'smooth', block: 'center'},
+        modalOverlayOpeningPadding:'20', // padding around highlighted element
+        popperOptions: { // moves tutorial box to the left
+          modifiers: [{ name: 'offset', options: { offset: [0,  52] } }]
+        }
+      },
+      exitOnEsc: false
+    })
+  
+    startTour(tour, steps[type])
+  }
+  
+  
+  function startTour(tour,steps){
+    for (i = 0; i < steps.length; i++) {
+      btns = [];
+      // no back button at the start
+      if (i > 0) {
+        btns.push({
+          text: 'Back',
+          classes: 'shepherd-button-secondary',
+          action: function () {
+            return tour.back();
+          }
+        });
+      }
+      // no next button on last step
+      if (i != (steps.length - 1)) {
+        btns.push({
+          text: 'Next',
+          classes: 'shepherd-button-primary '+steps[i].class+(steps[i].disabled ? ' disabled-button': ''),
+          disabled: steps[i].disabled,
+          action: function () {
+            return tour.next();
+          }
+        });
+      } else {
+        btns.push({
+          text: 'Close',
+          classes: 'shepherd-button-primary',
+          action: function () {
+            return tour.hide();
+          }
+        });
+      }
+  
+      let step = {
+        id: 'step-'+i,
+        attachTo: {
+          element: steps[i].id,
+          on: steps[i].location
+        },
+        text: steps[i].text,
+        title: steps[i].title,
+        buttons: btns,
+        cancelIcon: {
+          enabled: false
+        }
+      }
+  
+      tour.addStep(step);
+    }
+  
+    tour.start();
+  }
+
+function runTour(){
+    createTour([
+        {
+            "id": "#image",
+            "location": "bottom",
+            "title": "Transparency Slider",
+            "text": "Use the slider.",
+            "class": "main-vis-step",
+            "disabled": true
+        },
+        {
+            "id": "#image",
+            "location": "bottom",
+            "title": "Transparency Slider",
+            "text": "Great! Now close the window to proceed.",
+            "class": "main-vis-step",
+            "disabled": false
+        }
+    ])
+}
